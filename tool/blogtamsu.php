2fe465c85c62fe19c6c2c0a50a1c171d10eaa6c4
vlhay <bloggernamnguyen@gmail.com>
2018-12-08 14:47:00 +0700

    Create blogtamsu.php

---
+++ tool/blogtamsu.php
@@ -0,0 +1,61 @@
+<title>Tool Leech Blogtamsu.vn</title>
+<?php
+session_start();
+require 'head.php';
+if (!isset($_SESSION['domain']))
+{
+echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>Bạn chưa đăng nhập! <a href="/">Đăng nhập</a>';
+}
+elseif (!isset($_GET['url']))
+{
+echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><form method="get">Url: <input name="url" type="text"><br /><input type="submit" value="Leech" ></form>';
+}
+elseif ($_POST['submit']){
+$post = post_dl($_POST['title'],$_POST['url'],$_POST['content'], $_POST['tags'],$_POST['img_thumb'],$_POST['title_seo'],$_POST['link_down'],$_POST['desc'],$_POST['temp_index'],$_POST['box'],$_POST['phan_trang'],$_POST['submit']);
+echo $post;
+echo '<u>Tiếp tục</u>:<br /><form method="get">Url: <input name="url" type="text"><br /><input type="submit" value="Leech" ></form>';
+}
+else
+{
+$url = $_GET['url'];
+$url =  str_replace('http://m.','',$url);
+$url =  str_replace('http://','',$url);
+$url =  str_replace($url,'http://m.'.$url ,$url);
+$curl = curl_init();
+curl_setopt ($curl, CURLOPT_URL, $url);
+curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt ($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Linux; U; Android 4.1.2; vi; SAMSUNG Build/JZO54K) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 UCBrowser/9.7.5.418 U3/0.8.0 Mobile Safari/533.1');
+$lay = curl_exec($curl);
+$title = explode('<title>',$lay);
+$title = explode('</title>',$title[1]);
+$title = trim($title[0]);
+$title = explode('|',$title);
+$title = trim($title[0]);
+$lay = explode('<div class="remain_detail">',$lay);
+$lay = explode('<center><ins class="adsbygoogle"',$lay[1]);
+$lay = trim($lay[0]);
+$lay = strip_tags($lay,'<img>,<p>,<strong>,<em>,<div>');
+$lay = trim($lay);
+$lay = str_replace('(adsbygoogle = window.adsbygoogle || []).push({});','',$lay);
+$lay = preg_replace('/\s\s+/', ' ', trim($lay));
+curl_close($curl);
+echo '<form method="post">
+* Tiêu đề:<br/><input type="text" name="title" id="title" value="'.$title.'" required=""/><br/>
+URL:<br/> <input type="text" name="url" id="url" value=""/><br />
+* Nội dung:<br/><textarea name="content">'.$lay.'</textarea><br/>
+Từ khóa:(Cách nhau dấu ",")<br/>
+<input type="text" name="tags" value="'.$title.'"/><br/>
+Ảnh đại diện: <br/><input type="text" name="img_thumb" value=""/><br/>
+Title trên google:<br/><input type="text" name="title_seo" value=""/><br/>
+Link tải:(Dùng HTML)<br/>
+<textarea name="link_down" id="editor"></textarea><br/>
+Mô tả:<br/><input type="text" name="desc" value=""/><br/>
+Mục hiển thị:<br />
+'.$_SESSION['cm'].'
+<label for="phan_trang">
+<input type="checkbox" name="phan_trang" value="1"/> Phân trang
+</label><br/>
+<input type="submit" name="submit" value="Đăng bài"/>
+</form>';
+}
+?>
